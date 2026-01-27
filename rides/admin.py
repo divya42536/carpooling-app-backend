8d87f557d11f335846581d4145ae5691daf9e9b7
divya42536 <divya42536@gmail.com>
2026-01-27 13:38:30 -0800

    added changes

--- rides/admin.py
+++ rides/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from .models import Ride
-from .models import Booking
+# from .models import Ride
+# from .models import Booking
 
 # Register your models here.
-@admin.register(Ride)
-class Ride(admin.ModelAdmin):
-    list_display = (
-        "from",
-        "to",
-        )
+# @admin.register(Ride)
+# class Ride(admin.ModelAdmin):
+#     list_display = (
+#         "from",
+#         "to",
+#         )
--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Rider
 from .models import Driver
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
