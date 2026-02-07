2ea96b7aed84bef7eea469b03cce25992b7b51c7
divya42536 <divya42536@gmail.com>
2026-02-06 20:16:05 -0800

    Add django-cors-headers

--- carpooling/settings.py
+++ carpooling/settings.py
@@ -44,10 +44,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
-   'rest_framework',
+   
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
