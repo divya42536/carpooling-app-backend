ee99bd9e0f9f023bdaf9e6a620ecd271d6e93f5d
divya42536 <divya42536@gmail.com>
2026-02-06 22:38:36 -0800

    changed the carpooling/urls

--- carpooling/urls.py
+++ carpooling/urls.py
@@ -14,18 +14,52 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.http import JsonResponse
+
+# Optional: API root view to list all endpoints
+def api_root(request):
+    return JsonResponse({
+        "users": "/api/users/",
+        "rides": "/api/rides/",
+        "ratings": "/api/ratings/",
+        "bookings": "/api/bookings/",
+    })
 
 urlpatterns = [
+    # Admin
     path('admin/', admin.site.urls),
+
+    # App endpoints
     path('api/users/', include('users.urls')),
     path('api/rides/', include('rides.urls')),
     path('api/ratings/', include('ratings.urls')),
     path('api/bookings/', include('bookings.urls')),
 
+    # Redirect /api/ to API root JSON
+    path('api/', api_root),  # now visiting /api/ returns JSON of all endpoints
+
+    # Optional: redirect root '/' to /api/ or a welcome page
     path('', RedirectView.as_view(url='/api/', permanent=False)),
-    
-    
 ]
+
+
+
+# from django.contrib import admin
+# from django.urls import path, include
+# from django.views.generic import RedirectView
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('api/users/', include('users.urls')),
+#     path('api/rides/', include('rides.urls')),
+#     path('api/ratings/', include('ratings.urls')),
+#     path('api/bookings/', include('bookings.urls')),
+
+#     path('', RedirectView.as_view(url='/api/', permanent=False)),
+    
+    
+# ]
