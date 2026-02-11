676100685f90b2039d4f11a915f627b91e502f4b
geethavs88 <geethavs88@gmail.com>
2026-02-10 18:27:56 -0800

    fixed accept ride request

--- carpooling/settings.py
+++ carpooling/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-1=^20xf4yg0t%)+qa)z7rmm=1o6%gy6f(m@7c1hph_7xmo%5tz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['10.0.2.2']
+# ALLOWED_HOSTS = ['10.0.2.2']
 ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
